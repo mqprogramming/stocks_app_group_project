9d014234702b581ac339f788a4cbdeaf3cfd99cb
Steven Burrows <steven@Stevens-MacBook-Air.local>
2020-04-12 13:17:29 +0100

    Vue setup complete

--- client/src/pages/SearchPage/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './SearchPage.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#searchPage')
--- client/src/pages/StockDetailsPage/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './StockDetailsPage.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#stockDetailsPage')
