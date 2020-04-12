ab22a91e03acf324f3910e84843f479c1d049e87
Steven Burrows <steven@Stevens-MacBook-Air.local>
2020-04-12 14:15:03 +0100

    search form working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>app goes here</h2>
+    <stock-search />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import StockSearch from "./components/StockSearch"
 export default {
-  name: 'App',
+  name: 'App' ,
   components: {
-    HelloWorld
+    'stock-search': StockSearch
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ client/src/components/StockSearch.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+
+
+      <form v-on:submit.prevent="handleSubmit">
+
+      <label for="symbol">Enter the Stock Ticker Name:</label>
+    <input type="text" id="symbol" name="symbol" v-model="symbol" />
+
+      <input type="submit" name="submit" value="Search" />
+      </form>
+
+  </div>
+</template>
+
+<script>
+import { eventBus } from "@/main";
+export default {
+    name: 'stock-search',
+    data() {
+        return {
+            symbol: "",
+            
+        }
+    },
+    methods: {
+        handleSubmit () {
+            eventBus.$emit('search-submit', this.symbol);
+            this.symbol = "";
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
