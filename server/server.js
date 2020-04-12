4cc161fcd41641c60a3c6feb3a7ed0043d7d3693
mqprogramming <matt.qgley@gmail.com>
2020-04-12 14:16:23 +0100

    server setup

---
+++ server/server.js
@@ -0,0 +1,26 @@
+// Requiring dependencies
+const express = require('express');
+const app = express();
+
+const parser = require('body-parser');
+app.use(parser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+// Creating client
+const OurClient = require('mongodb').MongoClient;
+
+const createRouter = require('./helpers/create_router.js');
+
+OurClient.connect('mongodb://localhost:27017')
+  .then( (client) => {
+    const db = client.db(portfolio);
+    const portfolioCollection = db.collection('user_portfolio');
+    const portfolioRouter = createRouter(portfolioCollection);
+    app.use('/api/user-portfolio', portfolioRouter);
+  });
+
+app.listen(3000, function(){
+  console.log(`Listening on port ${this.address().port}`)
+});
