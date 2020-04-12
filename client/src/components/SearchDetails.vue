fadc6196308e2d1803fff73d4aa19e4af3d59b63
Steven Burrows <steven@Stevens-MacBook-Air.local>
2020-04-12 14:34:02 +0100

    search details WIP

---
+++ client/src/components/SearchDetails.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'search-details',
+    data() {
+        return {
+            stockDetails: []
+        }
+    },
+
+    mounted() {
+        fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${process.env.VUE_APP_API_KEY}`)
+        .then(res => res.json)
+        .then(stockData => this.stockDetails = stockData )
+    }
+
+}
+</script>
+
+<style>
+
+</style>
