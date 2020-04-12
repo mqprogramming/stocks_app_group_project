1d1b6e19c48a777f828bf3fe174a025ce2f31618
Anna <elbereth_spencer@hotmail.com>
2020-04-12 16:51:19 +0100

    fixing search form with eventBus

--- client/src/components/SearchDetails.vue
+++ client/src/components/SearchDetails.vue
@@ -1,36 +1,35 @@
 <template>
-  <div>
-      
-
-  </div>
+  <div></div>
 </template>
 
 <script>
-import { eventBus } from '@/main';
+import { eventBus } from "@/main";
 export default {
-    
-    name: 'search-details',
-    data() {
-        return {
-            stockDetails: {},
-            ticker: ""
-            
-        }
-    },
+  name: "search-details",
+  data() {
+    return {
+      stockDetails: {},
+      query:"",
+      ticker: ""
+    };
+  },
 
-    mounted() {
-
-
-        fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${process.env.VUE_APP_API_KEY}`)
+  mounted() {
+    eventBus.$on("search-submit", symbol => {
+      this.ticker = symbol;
+      this.query =
+        "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
+        symbol +
+        "&interval=5min&apikey=";
+      fetch(`${this.query}${process.env.VUE_APP_API_KEY}`)
         .then(res => res.json())
-        .then(stockData => this.stockDetails[0] = stockData )
-        console.log(`Output: `, this.stockDetails);
-        
-    }
+        .then(stockData => (this.stockDetails[0] = stockData));
+    });
 
-}
+    console.log(`Output: `, this.stockDetails);
+  }
+};
 </script>
 
 <style>
-
 </style>
