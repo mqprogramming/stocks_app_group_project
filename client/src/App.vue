f09c40e588784dd70f399b2f08fe143fe65dae10
Steven Burrows <steven@Stevens-MacBook-Air.local>
2020-04-12 15:13:54 +0100

    api fetch working

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <h2>app goes here</h2>
     <stock-search />
+    <search-details />
   </div>
 </template>
 
 <script>
 import StockSearch from "./components/StockSearch"
+import SearchDetails from "./components/SearchDetails"
 export default {
   name: 'App' ,
   components: {
-    'stock-search': StockSearch
+    'stock-search': StockSearch,
+    'search-details': SearchDetails
   }
 
 }
--- client/src/components/SearchDetails.vue
+++ client/src/components/SearchDetails.vue
@@ -1,22 +1,31 @@
 <template>
   <div>
+      
 
   </div>
 </template>
 
 <script>
+import { eventBus } from '@/main';
 export default {
+    
     name: 'search-details',
     data() {
         return {
-            stockDetails: []
+            stockDetails: {},
+            ticker: ""
+            
         }
     },
 
     mounted() {
+
+
         fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${process.env.VUE_APP_API_KEY}`)
-        .then(res => res.json)
-        .then(stockData => this.stockDetails = stockData )
+        .then(res => res.json())
+        .then(stockData => this.stockDetails[0] = stockData )
+        console.log(`Output: `, this.stockDetails);
+        
     }
 
 }
