677bee948101a86420f5781bee44d0fc488b9d6f
Anna <elbereth_spencer@hotmail.com>
2020-04-13 13:56:00 +0100

    serch feature finished

--- client/src/components/SearchDetails.vue
+++ client/src/components/SearchDetails.vue
@@ -1,36 +1,41 @@
 <template>
   <div>
-      <p v-if="stockDetails">{{ stockDetails }}</p>
+    <p v-if="stockDetails">{{ stockDetails }}</p>
   </div>
 </template>
 
 <script>
-import { eventBus } from '@/main';
+import { eventBus } from "@/main";
 export default {
-    
-    name: 'search-details',
-    data() {
-        return {
-            stockDetails: {},
-            ticker: ""
-        }
-    },
-    mounted() {
+  name: "search-details",
+  data() {
+    return {
+      stockDetails: {},
+      ticker: "",
+      query: ""
+    };
+  },
+  mounted() {
+    eventBus.$on("search-submit", symbol => {
+      this.ticker = symbol;
+      this.query =
+        "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
+        symbol +
+        "&interval=5min&apikey=";
+      const request = async () => {
+        const response = await fetch(
+          `${this.query}${process.env.VUE_APP_API_KEY}`
+        );
+        const json = await response.json();
+        this.stockDetails = json;
+        console.log(json["Meta Data"]);
+      };
 
-        const request = async () => {
-            const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${process.env.VUE_APP_API_KEY}`);
-            const json = await response.json();
-            this.stockDetails = json;
-            console.log(json['Meta Data']);
-        }
-
-        request();
-        
-    },
-
-}
+      request();
+    });
+  }
+};
 </script>
 
 <style>
-
 </style>
