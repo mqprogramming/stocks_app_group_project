cba7b9fb50708d6b941255d8a2b6366376ceaddb
Stephen <st-jeeves@hotmail.co.uk>
2020-04-14 17:51:59 +0100

    finished setting up details and grid, working on delete button

--- client/src/components/PortfolioDetails.vue
+++ client/src/components/PortfolioDetails.vue
@@ -2,26 +2,42 @@
   <div>
     <h3>{{ stock.ticker}}</h3>
     <p> Price: {{ stock.price }}</p>
-    <p>Quantity: {{ stock.quantity }}</p>
+    <p> Quantity: {{ stock.quantity }}</p>
     <p> Date: {{ stock.date_and_time }}</p>
-    <p> <button v-on:click="handleSell(stock._id)">Sell</button> </p>
+    <p> <button type="button" v-on:click="deletePortfolio(stock._id)">Sell</button> </p>
   
   </div>
 </template>
 
 <script>
 import { eventBus } from '@/main.js'
-import PortfolioService from '@/services/PortfolioService.js'
+import PortfolioService from '../../services/PortfolioService'
 
 export default {
   name: 'portfolio-details',
-  props: ['game'],
+  props: ['stock'],
   methods: {
-    deletePortfolio(){
+    deletePortfolio(id){
       PortfolioService.deletePortfolio(this.portfolio._id)
       .then(() => eventBus.$emit('portfolio-deleted', this.portfolio._id))
     }
+    // handleSell(id) {
+    //   console.log(id);
+      
+    //   PortfolioService.deletePortfolio(id).then ((reponce) => {
+    //     let index = this.portfolio.findIndex(shares => shares._id == id)
+    //   this.portfolio.splice(index, 1)
+    //   })
+    // }
+  //   handleSell(id) {
+
+  //   deletePortfolio(){
+  //     PortfolioService.deletePortfolio(this.portfolio._id)
+  //     .then(() => eventBus.$emit('portfolio-deleted', this.portfolio._id))
+  //   }
+  // }
   }
+  
 
 }
 </script>
--- client/src/components/PortfolioGrid.vue
+++ client/src/components/PortfolioGrid.vue
@@ -2,14 +2,17 @@
   <div class="details">
     <h1>Portfolio</h1>
     <!-- <h2>All the details!</h2> -->
-      <portfolio-details v-for="stock in portfolio" v-bind:key="stock._id"/>
-        <!-- <li>Ticker: {{ stock.ticker }}</li>
-        <li>Name: {{ stock.name }}</li>
+    <!-- <ul> -->
+      <portfolio-details class="shares" v-for="stock in portfolio" :key="stock._id" :stock="stock"/>
+    <!-- </ul> -->
+       <!-- <li>Ticker: {{ stock.ticker }}</li> -->
+        <!--  <li>Name: {{ stock.name }}</li>
         <li> Price: {{ stock.price }}</li>
         <li>Quantity: {{ stock.quantity }}</li>
         <li> Date: {{ stock.date_and_time }}</li>
-        <li> <button v-on:click="handleSell(stock._id)">Sell</button> </li>
-      </ul> -->
+        <li> <button v-on:click="handleSell(stock._id)">Sell</button> </li> -->
+      <!-- </ul>  -->
+      
   </div>
 </template>
 
@@ -20,6 +23,7 @@ import PortfolioDetails from './PortfolioDetails'
 
 export default {
   name: "portfolio-grid",
+ 
   data() {
     return {
       portfolio: []
@@ -44,32 +48,26 @@ export default {
   mounted() {
     this.fetchData()
 
-    eventBus.$on('buy-shares', (shares) => {this.portfolio.push(shares)
+    eventBus.$on('buy-shares', (stock) => {
+      this.portfolio.push(stock)
     })
 
     eventBus.$on('portfolio-deleted', (id) => {
-      let index = this.portfolio.findIndex(shares => shares._id == id)
-    this.portfolio.splice(index, 1)
+      let index = this.portfolio.findIndex(stock => stock._id === id)
+      this.portfolio.splice(index, 1)
     })
-
-    // eventBus.$on('delete-shares', (id) => {
-      
-    // })
-
-    
 },
 
 components: {
       'portfolio-details': PortfolioDetails
     }
-
     
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
+/* h3 {
   margin: 40px 0 0;
 }
 ul {
@@ -82,5 +80,25 @@ li {
 }
 a {
   color: #42b983;
+} */
+
+.details {
+  display: flex;
+}
+
+.header {
+  flex: 1;
+  width: 100%;
+}
+
+.shares {
+  flex: 2;
+  margin: 5px;
+  border-style: solid;
+  border-color: aliceblue;
+  background-color: #1C3144;
+  width: 200px;
+  text-align: center;
+  
 }
 </style>
