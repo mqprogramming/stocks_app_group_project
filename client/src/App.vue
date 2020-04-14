30dd6699722a58411044f432c8570c86a4a1c863
Anna <elbereth_spencer@hotmail.com>
2020-04-14 12:47:27 +0100

    adding total stock value component

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,6 +4,7 @@
     <stock-search />
     <search-details />
     <portfolio-chart />
+    <portfolio-total-value/>
     <portfolio-details/>
   </div>
 </template>
@@ -13,14 +14,15 @@ import StockSearch from "./components/StockSearch"
 import SearchDetails from "./components/SearchDetails"
 import PortfolioChart from "./components/PortfolioChart"
 import PortfolioDetails from "./components/PortfolioDetails"
-
+import PortfolioTotalValue from "./components/PortfolioTotalValue"
 export default {
   name: 'App' ,
   components: {
     'stock-search': StockSearch,
     'search-details': SearchDetails,
     'portfolio-chart': PortfolioChart,
-    'portfolio-details': PortfolioDetails
+    'portfolio-details': PortfolioDetails,
+    'portfolio-total-value': PortfolioTotalValue
   }
 
 }
---
+++ client/src/components/PortfolioTotalValue.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <header>
+      
+      <h2>Total portfolio: £ {{ totalBalances }}</h2>
+      
+    </header>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PortfolioTotalValue",
+  data() {
+    return {
+      total: "",
+      portfolio: "",
+      tickerList: []
+    };
+  },
+  computed: {
+    totalBalances: this.totalPrice()
+  },
+  methods: {
+    getLatestPrice: function(symbol) {
+      this.query =
+        "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
+        symbol +
+        "&interval=5min&apikey=";
+      const request = async () => {
+        const response = await fetch(
+          `${this.query}${process.env.VUE_APP_API_KEY}`
+        );
+        const json = await response.json();
+        const stockDetails = json["Meta Data"];
+        const stockTimeSeries = json["Time Series (5min)"];
+        const latestPrice = Object.values(this.stockTimeSeries)[0]["4. close"];
+        return latestPrice;
+      };
+    },
+    retrieveTickerList: function() {
+      fetch("http://localhost:3000/api/shares-portfolio")
+        .then(response => response.json())
+        .then(portfolio => (this.portfolio = portfolio));
+      for (var stock in this.porfolio) {
+        this.tickerList[stock.ticker]=stock.quantity;
+      }
+    },
+    totalPrice: function() {
+        var totalPortfolioValue=0;
+        this.retrieveTickerList();
+        const tickers= Object.keys(this.tickerList);
+        for(var stockName in tickers){
+            const currentPrice= this.getLatestPrice(stockName);
+            totalPortfolioValue+=currentPrice*this.tickerList[stockName];
+        }
+        return totalPortfolioValue;
+    }
+  }
+};
+</script>
+
+
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
