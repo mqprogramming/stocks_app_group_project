950f608509ccef36ca83399a0aaa0b1be17a092c
mqprogramming <matt.qgley@gmail.com>
2020-04-14 14:44:22 +0100

    new functions, wip

--- client/src/components/PortfolioChart.vue
+++ client/src/components/PortfolioChart.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Chart</h1>
+    <button v-on:click="this.sortPortfolioByDate">Sort Portfolio By Date</button>
+    <button v-on:click="this.createDatesArray">Create Dates Array</button>
     <highcharts :constructor-type="'stockChart'" :options="chartOptions"></highcharts>
   </div>
 </template>
@@ -12,11 +14,13 @@ import stockInit from 'highcharts/modules/stock'
  
 stockInit(Highcharts)
 
-
   export default {
     name: 'portfolio-chart',
     data(){
       return {
+        portfolioDetails: [],
+        datesArray: [],
+
         chartOptions: {
           title: {
             text: 'Test Title'
@@ -24,6 +28,7 @@ stockInit(Highcharts)
           series:
             [
               {
+                // Test Data
                 name: 'APPLE',
                 data: [
                   [new Date('2015-08-01').getTime(), 25],
@@ -32,7 +37,7 @@ stockInit(Highcharts)
                   [new Date('2015-08-05').getTime(), 21],
                   [new Date('2015-09-01').getTime(), 25],
                   [new Date('2015-10-02').getTime(), 27],              
-                  [new Date('2015-11-03').getTime(), 29]
+                  [new Date('2018-04-08 13:25:00').getTime(), 29]
                 ]
               },
               {
@@ -63,8 +68,81 @@ stockInit(Highcharts)
         }
       }
     },
+    mounted() {
+      this.fetchData();
+    },
+    methods: {
+      // Fetches portfolio data.
+      fetchData() {
+        fetch("http://localhost:3000/api/shares-portfolio")
+          .then(response => response.json())
+          .then(data => (this.portfolioDetails = data));
+      },
+      // Sorts array of objects by key.
+      sortingFunction(key, order = 'asc') {
+        return function innerSort(a, b) {
+          if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            // property doesn't exist on either object
+            return 0;
+          }
+
+          const varA = (typeof a[key] === 'string')
+            ? a[key].toUpperCase() : a[key];
+          const varB = (typeof b[key] === 'string')
+            ? b[key].toUpperCase() : b[key];
+
+          let comparison = 0;
+          if (varA > varB) {
+            comparison = 1;
+          } else if (varA < varB) {
+            comparison = -1;
+          }
+          return (
+            (order === 'desc') ? (comparison * -1) : comparison
+          );
+        };
+      },
+      // Sorts portfolio objects by date & time.
+      sortPortfolioByDate() {
+        this.portfolioDetails.forEach((record) => {
+          let unix_time = new Date(record['date_and_time']).getTime();
+          record['unix_time'] = unix_time;
+        });
+        this.portfolioDetails.sort(this.sortingFunction('unix_time'));
+
+        console.log(this.portfolioDetails);
+      },
+      // Creates an array of dates from oldest portfolio record to current day.
+      createDatesArray() {
+        function appendLeadingZeroes(n){
+          if(n <= 9){
+            return "0" + n;
+          }
+          return n
+        };
+
+        function formatDate(date){
+          return date.getFullYear() + "-" + appendLeadingZeroes(date.getMonth() + 1) + "-" + appendLeadingZeroes(date.getDate());
+        };
+
+        let current_datetime = new Date();
+        let formatted_date = formatDate(current_datetime);
+        let last_pushed_date = this.portfolioDetails[0]['date_and_time'];
+
+        while (new Date(formatted_date).getTime() > new Date(last_pushed_date).getTime()) {
+          let last_pushed_date_unix = new Date(last_pushed_date).getTime();
+          let to_push_date_unix = last_pushed_date_unix + 86400000; // plus one day in ms
+          let to_push_date = formatDate(new Date(to_push_date_unix));
+          this.datesArray.push(to_push_date);
+          last_pushed_date = to_push_date;
+        };
+
+        console.log(this.datesArray);
+      },
+
+    },
     components: {
-      highcharts: Chart 
+      highcharts: Chart
     }
   }
 </script>
--- client/src/components/PortfolioDetails.vue
+++ client/src/components/PortfolioDetails.vue
@@ -12,6 +12,7 @@
 </template>
 
 <script>
+
 export default {
   name: "portfolio-details",
   data() {
